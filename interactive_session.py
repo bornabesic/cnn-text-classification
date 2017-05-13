dc7ec9a1e9a2994c41198bbfac0fc2ca5d84507f
Borna Bešić <besic.borna@gmail.com>
2017-05-13 16:19:17 +0200

    Show class probabilities during an interactive session

--- interactive_session.py
+++ interactive_session.py
@@ -32,15 +32,25 @@ with tf.Session() as sess:
 	print("Classes:")
 	for c in class_dict:
 		print(c)
+	print()
 
 	reverse_class_dict=dict()
 	for k in class_dict.keys():
 		reverse_class_dict[tuple(class_dict[k])]=k
+	
+	try:
+		while True:
+			sentence = input("> ")
+			word_indices = data.index_and_align(data.process_sentence(sentence), max_sentence_length)
+			output, prediction = neural_network.feed([word_indices])
+			probabilities = sess.run(tf.nn.softmax(output))[0]
 
-	while True:
-		sentence = input("> ")
-		word_indices = data.index_and_align(data.process_sentence(sentence), max_sentence_length)
-		_, prediction = neural_network.feed([word_indices])
-		index = int(prediction[0])
-		one_hot=tuple([0 if i!=index else 1 for i in range(num_classes)])
-		print(reverse_class_dict[one_hot])
+			max_index = int(prediction[0])
+
+			for i in range(num_classes):
+				one_hot=tuple([0 if j!=i else 1 for j in range(num_classes)])
+				print("{}\t{}\t{}".format(reverse_class_dict[one_hot], probabilities[i], "*" if i==max_index else ""))
+			print()
+
+	except KeyboardInterrupt:
+		pass
